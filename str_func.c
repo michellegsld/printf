00361635645715d8037adb4eac2c5222bc8ca7d7
Michelle Giraldo <michellegsld@gmail.com>
2019-10-28 16:55:29 -0400

    3. Pushing, Removing case 'p', Removing the variable len and corresponding code, Removing the initialization of null bytes, michellegsld

--- str_func.c
+++ str_func.c
@@ -4,33 +4,21 @@
  * simstr_func - Function to produce output for conversion specifiers c, s & p
  * @c: The specific conversion specifier
  * @v_out: Where the output is saved to
- * @var: It's contents only matters for c, s while p needs it's address
- *
- * Return: Nothing
+ * @var: What will be added to v_out
  */
 void simstr_func(char *c, char *v_out, char *var)
 {
-	int i, j, k;
-	int len = 0;
-	char *add = (char *)&var;
+	int i;
 
 	switch (*c)
 	{
 		case 'c':
 			v_out[0] = var[0];
-			v_out[1] = '\0';
 			break;
 		case 's':
-			for (i = 0; add[i] != '\0'; i++)
-				len++;
-			for (j = 0; j <= len; j++)
-				v_out[j] = add[j];
+			for (i = 0; var[i] != '\0'; i++)
+				v_out[i] = var[i];
 			break;
-		case 'p':
-			for (i = 0; add[i] != '\0'; i++)
-				len++;
-			for (k = 0; v_out[k] <= len; k++)
-				v_out[k] = add[k];
 		default:
 			break;
 	}
@@ -39,11 +27,8 @@ void simstr_func(char *c, char *v_out, char *var)
 /**
  * per_func - Function to produce conversion specifier %
  * @v_out: Where output is saved to
- *
- * Return: Nothing
  */
 void per_func(char *v_out)
 {
 	v_out[0] = '%';
-	v_out[1] = '\0';
 }
