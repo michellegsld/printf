9599aa5113be3753ad37d5ed19bcedde6f8765ee
jenntang1 <1039@holbertonschool.com>
2019-10-28 21:17:10 +0000

    4. Pushing, Updating to produce separate functions for conversion specifiers: c, s and %, jenntang1

--- str_func.c
+++ str_func.c
@@ -1,32 +1,33 @@
 #include "holberton.h"
 
 /**
- * simstr_func - Function to produce output for conversion specifiers c, s & p
- * @c: The specific conversion specifier
- * @v_out: Where the output is saved to
- * @var: What will be added to v_out
+ * char_func - Function to produce output for conversion specifier: c
+ * @v_out: variable output
+ * @var: pass-in variable
  */
-void simstr_func(char *c, char *v_out, char *var)
+void char_func(char *v_out, char var)
+{
+	v_out[0] = var[0];
+}
+
+/**
+ * string_func - Function to produce output for conversion specifier: s
+ * @v_out: variable output
+ * @var: pass-in variable
+ */
+void string_func(char *v_out, char *var)
 {
 	int i;
 
-	switch (*c)
+	for (i = 0; var[i] != '\0'; i++)
 	{
-		case 'c':
-			v_out[0] = var[0];
-			break;
-		case 's':
-			for (i = 0; var[i] != '\0'; i++)
-				v_out[i] = var[i];
-			break;
-		default:
-			break;
+		v_out[i] = var[i];
 	}
 }
 
 /**
- * per_func - Function to produce conversion specifier %
- * @v_out: Where output is saved to
+ * per_func - Function to produce output for conversion specifier: %
+ * @v_out: variable output
  */
 void per_func(char *v_out)
 {
