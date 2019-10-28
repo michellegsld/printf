26360d8c36679067d59b72a83d5db98c511647b4
Michelle Giraldo <michellegsld@gmail.com>
2019-10-28 01:57:16 -0400

    2. Pushing, Inserting all prototypes believed to be needed, Including comments specifying most prototypes in more details, Fixing spacing between prototypes to become more human-readable, michellegsld

--- holberton.h
+++ holberton.h
@@ -6,13 +6,12 @@
 #include <unistd.h>
 #include <stdarg.h>
 
-#define BUFFER 1024
-/* ui = (unsigned int)INT_MAX + 1024; */
+#define BUFSIZE 1024
 
 /**
- * struct - Struct for flag characters
- * @charfunc: flag characters 
- * @f: function
+ * struct - Struct for flag/conversion characters
+ * @charfunc: Specific flag/conversion character
+ * @f: The corresponding function name
  */
 typedef struct
 {
@@ -20,16 +19,39 @@ typedef struct
 	char (*f)(char);
 } charfunc;
 
-/* int _strlen(char *s); 0x05 #2 get string length */
-/* void _putchar(char *s); 0x08 #0 print char */
-/* void print_rev(char *s); 0x05 #4 print string in reverse */
-/* char *rot13(char *encode); 0x06 #8 print rot13 for encrytion */
+/* int _strlen(char *s); 0x05 #2 get string length (TO BE INCLUDED)*/
+/* void _putchar(char *s); 0x08 #0 print char  (TO BE INCLUDED)*/
+/* void print_rev(char *s); 0x05 #4 print string in reverse  (WRITTEN BELOW)*/
+/* char *rot13(char *encode); 0x06 #8 print rot13 for encrytion  (WRITTEN BELOW)*/
+
+/* char precision_func(char *c, char *v_out, char *var); Depends on 'f' and '.' (TO BE INCLUDED)*/
+
+
+void str_concat(char *v_out);
+
+/* String prototypes: */
+void simstr_func(char *c, char *v_out, char *var);	//still need to fix the 'p' case
+void per_func(char *v_out);
+void rev_func(char *v_out, char *var);			//will reverse *var into *v_out: called for 'r'
+void rot13_func(char *v_out, char *var);		//will rot13 *var into *v_out: called for 'R'
+
+/* Number prototypes: */	//would these *var be chars or were they supposed to be num?
+				//maybe within func... they are done atoi() then the num is convert.
+void hex_func(char *c, char *v_out, char *var);		//unsure about return type: called for 'x' 'X'
+void oct_func(char *v_out, char *var);			//unsure about return type and char *c: called for 'o'
+void undec_func(char *v_out, char *var);		//^: called for 'u'
+void bin_func(char *v_out, char *var);			//^^: called for 'b'
+void dec_func(char *c, char *v_out, char *var);		//unsure about return type: called for 'd' 'i'
+
+/* Flag prototypes: */		//unsure about the need of *var in any of these
+				//the *c is important ==> will be the con. specifier, decides if this works?
+				//do we need to send in another variable that accounts for field width?
+void flag_zero(char *c, char *v_out, char *var, int width);	//including width as output depends on it
+void flag_just(char *c, char *v_out, char *var, int width);	//^
+void flag_pound(char *c, char *v_out, char *var);
+void flag_plus(char *c, char *v_out, char *var);
+void flag_space(char *c, char *v_out, char *var);
 
 int _printf(const char *format, ...);
-/* char flag_zero(char); may use */
-/* char flag_dash(char); may use */
-/* char flag_pound(char); may use */
-/* char flag_pos(char); may use */
-/* char flag_space(char); may use */
 
 #endif /* HOLBERTON_H */
