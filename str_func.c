77fb982ae0117b307b18e83ca66708e0a9d9d0e3
Michelle Giraldo <michellegsld@gmail.com>
2019-10-28 22:37:58 -0400

    5. Pushing, Updating the variable var in char_func to recognize it as a single char, michellegsld

---
+++ str_func.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * char_func - Function to produce output for conversion specifier: c
+ * @v_out: variable output
+ * @var: pass-in variable
+ */
+void char_func(char *v_out, char var)
+{
+	v_out[0] = var;
+}
+
+/**
+ * string_func - Function to produce output for conversion specifier: s
+ * @v_out: variable output
+ * @var: pass-in variable
+ */
+void string_func(char *v_out, char *var)
+{
+	int i;
+
+	for (i = 0; var[i] != '\0'; i++)
+	{
+		v_out[i] = var[i];
+	}
+}
+
+/**
+ * per_func - Function to produce output for conversion specifier: %
+ * @v_out: variable output
+ */
+void per_func(char *v_out)
+{
+	v_out[0] = '%';
+}
